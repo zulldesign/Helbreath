a34724d7e105c608c859fabc5a782d13c314a0e7
zulldesign <admin@zulldesign.ml>
2014-09-28 00:42:06 +0800

    l

---
+++ Helbreath/Helbreath/Controllers/basicintroController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Helbreath.Controllers
+{
+    public class basicintroController : Controller
+    {
+        //
+        // GET: /basicintro/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
