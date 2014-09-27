1477444f60f4aeae4ef80502924dde906b68d5b1
zulldesign <admin@zulldesign.ml>
2014-09-27 23:50:07 +0800

    l

---
+++ Helbreath/Helbreath/Controllers/storylineController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Helbreath.Controllers
+{
+    public class storylineController : Controller
+    {
+        //
+        // GET: /storyline/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
